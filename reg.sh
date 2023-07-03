8be57ca43fb9f846288960e2de7d7669bf42097e
Yy <i@hiwer.cn>
2023-07-03 19:40:50 +0800

    更新 reg.sh

--- reg.sh
+++ reg.sh
@@ -1,3 +1,3 @@
-echo "$1"
-# sed -i 's/{{USERNAME}}/'"$1"'/g' reg.sh
-# ./MinecraftClient-20230623-225-linux-x64 reg.ini
+# echo "$1"
+sed -i 's/{{USERNAME}}/'"$1"'/g' reg.ini
+./MinecraftClient-20230623-225-linux-x64 reg.ini
