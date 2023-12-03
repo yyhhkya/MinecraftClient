f588374659f322e14306139f98599ef1f944eab1
Yy <87574541+yyhhkya@users.noreply.github.com>
2023-12-03 22:00:16 +0800

    更新

--- reg.sh
+++ reg.sh
@@ -1,2 +1,4 @@
-sed -i 's/{{USERNAME}}/'"$1"'/g' reg.ini
-./MinecraftClient-20231120-234-linux-x64 reg.ini
+# sed -i 's/{{USERNAME}}/'"$1"'/g' reg.ini
+# ./MinecraftClient-20231120-234-linux-x64 reg.ini
+
+echo {{DC_TOKEN}}
